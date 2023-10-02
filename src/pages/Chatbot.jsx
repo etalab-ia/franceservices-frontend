3e6b736fe0659455889a4de772539bf9d0f98327
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-02 14:30:29 +0200

    Banner component

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
-import { UserTools } from '../components/UserTools';
-import { Agent } from '../components/Agent';
-import { UserMessage } from '../components/UserMessage';
 import { useReducer } from 'react';
 import { initialState, reducer } from '../utils/reducer';
+import { Banner } from '../components/Banner';
+import { Chat } from '../components/Chat';
+import { UserTools } from '../components/UserTools';
 
 export function Chatbot() {
 
 	const   [state, dispatch] = useReducer(reducer, initialState);
 
-	console.log('chat')
-
 	return (
-		<div className="wrapper-container">
-			<UserTools />
-			<div className="flex flex-col items-center justify-end h-full mx-auto max-w-screen-xl">
-				<Agent
-					state={state}
-					dispatch={dispatch}
-				/>
-				<UserMessage 
-					state={state}
-					dispatch={dispatch}
-				/>
-			</div>
+		<div className='wrapper-container'>
+			<Banner />
+			<Chat state={state} dispatch={dispatch} />
+			{/* <UserTools state={state} dispatch={dispatch} /> */}
 		</div>
 	);
 }
