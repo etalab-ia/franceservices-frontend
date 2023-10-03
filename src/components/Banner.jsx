1949ffd03b7f7e54326610d9b0efa527cdacf5c3
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-03 12:36:11 +0200

    Add breadcrumb component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { chatbotProps } from '../constants/chatbotProps';
+import { Breadcrumb } from "@codegouvfr/react-dsfr/Breadcrumb";
 
 export function Banner() {
 
 	return (
 		<div className='banner-bot'>
-			<h1 className='main-title'>{chatbotProps.mainTitle}</h1>
+			<div>
+				<h1 className='main-title'>{chatbotProps.mainTitle}</h1>
+				<Breadcrumb className='breadcrumb-container'
+					currentPageLabel="Chat administratif"
+					homeLinkProps={{ to: '/' }}
+					segments={[{
+							label: 'Trouver des informations',
+							linkProps: {}
+					},]}
+				/>
+			</div>
 			<p className='subtitle'>{chatbotProps.subtitle}</p>
 		</div>
 	);
