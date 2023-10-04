8b8f21f40d3a65f0a3fde45d7caa019603187138
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-04 10:44:05 +0200

    Side buttons animation

---
+++ src/constants/sideButtons.jsx
@@ -0,0 +1,11 @@
+import saved from "../../icons/usertools/saved.svg"
+import history from "../../icons/usertools/history.svg"
+import conversation from "../../icons/usertools/conversation.svg"
+import question from "../../icons/usertools/question.svg"
+
+export const	sideButtonsProps = [
+	{ image: conversation, alt: "Bouton de conversation"},
+	{ image: history, alt: "Bouton d'historique"},
+	{ image: saved, alt: "Bouton de sauvegarde"},
+	{ image: question, alt: "Bouton d'informations"},
+]
