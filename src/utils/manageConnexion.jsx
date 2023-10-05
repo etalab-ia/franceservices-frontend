8ce88cc2b88cf3b63478ed6d41b0ef22bcccf1c8
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-05 19:22:15 +0200

    clean root component + keep username in localStorage

--- src/utils/manageConnexion.jsx
+++ src/utils/manageConnexion.jsx
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom";
 export const    handleSignout = async(state, dispatch) => {
 	await useFetch(signoutUrl, 'POST', {headers: { "Authorization": `Bearer ${state.userToken}`}})
 		.then(localStorage.removeItem('authToken'))
+		.then(localStorage.removeItem('username'))
 		.then(res => dispatch({ type: 'LOGOUT', nextUserToken: res.userToken }));
 
 	return <Navigate to="/" />
