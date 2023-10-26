7f999c75223171d3a2db4b3102fdb889feacc2d9
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-26 16:35:56 +0200

    Fix quality tags while archiving

--- src/components/Feedbacks/UserFeedbackSatisfaction.jsx
+++ src/components/Feedbacks/UserFeedbackSatisfaction.jsx
@@ -1,11 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { primaryButtons, secondaryButtons } from "../../constants/feedback";
-import { animated } from '@react-spring/web'
 import { NOT_SET } from "../../constants/status";
 
-export function UserFeedbackSatisfaction(props) {
-
-	const	{ isFirst, isConfirmed } = props;
+export function UserFeedbackSatisfaction({ isFirst, isConfirmed }) {
 	const	user = useSelector((state) => state.user);
 	const	dispatch = useDispatch();
 	const	buttons = isFirst ? primaryButtons : secondaryButtons;
@@ -17,16 +14,18 @@ export function UserFeedbackSatisfaction(props) {
 			dispatch({ type: 'SET_USER_CHOICES', nextKey: 'feedback', nextValue: index });
 	}
 
+	console.log('is conf: ', isConfirmed)
+
 	return (
 		<div className="row-message">
 			{buttons.map((button, index) => {
-				return <animated.button title={button.type} onClick={() => handleClick(index)} key={index} 
+				return <button title={button.type} onClick={() => handleClick(index)} key={index} 
 							className={`user-feedback-buttons border-solid ${index === user.choices.feedback ? 'bg-purple' : 'bg-white'}`}
 							disabled={isConfirmed}
 					>
 						<img className="mr-2" style={{ filter: index === user.choices.feedback ? "brightness(0) invert(1)" : "none" }} src={button.img}/>
 						<p className={`${index === user.choices.feedback ? 'text-white' : 'text-purple'}`}>{button.name}</p>
-					</animated.button>
+					</button>
 			})}
 		</div>
 	);
