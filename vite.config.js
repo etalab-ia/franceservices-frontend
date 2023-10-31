3ae08235c191e48d271b622261e0b1fa97859b61
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-10-31 11:17:37 +0100

    update vite config depending on prod/dev mode

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    cssCodeSplit: false,
-  },
-  server: {
-    watch: {
-      usePolling: true,
+export default ({ mode }) => {
+  const isProduction = mode === 'production';
+  const base = isProduction ? '' : '/albert';
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    build: {
+      outDir: 'dist',
+      cssCodeSplit: false,
     },
-    host: true, 
-    strictPort: true,
-    port: 4173,
-  },
-  base: ''
-})
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 4173,
+    },
+    base: base
+  });
+};
