abdde4f9b1685fa999cf80731996b58c6a3a4931
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-03 15:25:16 +0100

    Fix chat access when authToken no longer exist

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -2,8 +2,12 @@ export function checkConnexion(dispatch) {
 	const	authToken = localStorage.getItem('authToken');
 	const	username = localStorage.getItem('username');
 
-	if (authToken) {
+	if (authToken !== 'undefined') {
+		console.log(authToken)
 		dispatch({ type: 'LOGIN', nextUserToken: authToken });
 		dispatch({ type: 'SET_USERNAME', nextUsername: username });
+	}
+	else {
+		dispatch({ type: 'LOGOUT' })
 	}
 }
