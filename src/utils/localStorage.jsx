8456abfe034f98deba769da1932b6a6ddb282950
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-08 09:58:00 +0100

    Fix auth

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -2,7 +2,7 @@ export function checkConnexion(dispatch) {
 	const	authToken = localStorage.getItem('authToken');
 	const	username = localStorage.getItem('username');
 
-	if (authToken !== 'undefined') {
+	if (authToken !== 'undefined' && authToken != null) {
 		dispatch({ type: 'LOGIN', nextUserToken: authToken });
 		dispatch({ type: 'SET_USERNAME', nextUsername: username });
 	}
