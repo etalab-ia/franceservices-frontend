4c96ecd29bd814c01a3e54b32825fd22de57f9ea
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-13 15:50:02 +0100

    Fix auth + no response regeneration from archive tab

--- src/components/Global/QuestionnaireBot.jsx
+++ src/components/Global/QuestionnaireBot.jsx
@@ -1,4 +1,3 @@
-import { UserChatTools } from "../User/UserChatTools";
 import { Avatar } from "../Chat/Avatar";
 import { BotQuestion } from "./BotQuestion";
 
@@ -6,8 +5,7 @@ export function	QuestionnaireBot({ id, question, type, choice }) {
 
     return (
 		<div className="col-message">
-			<div className="row-message">
-				<UserChatTools type={type}/>
+			<div className="row-message ml-[110px]">
 				<Avatar user='agent' />
 				<div className='ml-4'>
 					<div className="py-4">{question}</div>
