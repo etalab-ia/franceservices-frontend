5a518bb9a1310cf9d5797b5c9ff2128e83211337
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-16 12:44:59 +0100

    css

--- src/components/Sheets/DisplaySheets.jsx
+++ src/components/Sheets/DisplaySheets.jsx
@@ -11,8 +11,7 @@ export function	DisplaySheets() {
 	return (
 		<>
 			{sheets.length ?
-				<div className='w-[700px] pr-6 pt-2 max-h-[610px] overflow-y-auto overflow-x-hidden'>
-					<h1 className='color-[#161616] leading-7'>Fiches service-public.fr associées</h1>
+				<div className='mt-5 w-[700px] pr-6 pt-2 max-h-[610px] overflow-y-auto overflow-x-hidden'>
 						{sheets.map((sheet, index) => {
 							return <div className="sheets-container" key={index}>
 								<TagSheets sheet={sheet} sheetId={getSheetId(sheet.url)}/>
