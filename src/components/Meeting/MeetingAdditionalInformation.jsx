2cdf3b3592f8767214cf561938115b3953800882
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-21 15:18:59 +0100

    Split meeting component - add tags

--- src/components/Meeting/MeetingAdditionalInformation.jsx
+++ src/components/Meeting/MeetingAdditionalInformation.jsx
@@ -1,15 +1,12 @@
-import Input from "@codegouvfr/react-dsfr/Input";
 import { GlobalColContainer } from "../Global/GlobalColContainer";
 import { GlobalSubtitle } from "../Global/GlobalSubtitle";
 import { meetingInformations } from "../../constants/meeting";
+import { MeetingAdditionalInput } from "./MeetingAdditionalInput";
 
 export function MeetingAdditionalInformations() {
+
 	return <GlobalColContainer>
 		<GlobalSubtitle>{meetingInformations}</GlobalSubtitle>
-		<Input
-			className="fr-mb-3w"
-			label="Thèmes associés"
-		/>
-		<Input className="fr-mb-3w" label="Administrations concernées"/>
+		<MeetingAdditionalInput />
 	</GlobalColContainer>
 }
