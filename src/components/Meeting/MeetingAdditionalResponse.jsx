2594877efda4c99f3bc5ead6312e2464264f1465
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-23 12:47:42 +0100

    apiBase

--- src/components/Meeting/MeetingAdditionalResponse.jsx
+++ src/components/Meeting/MeetingAdditionalResponse.jsx
@@ -5,7 +5,7 @@ import { MeetingTiles } from "./MeetingTiles";
 export function MeetingAdditionalResponse() {
 	return <div className="w-1/3">
 		<GlobalColContainer>
-		    <h3 className="text-2xl font-bold fr-pt-3w fr-pb-3w">Liens pratiques</h3>
+		    <h3 className="text-2xl font-bold fr-pb-3w">Liens pratiques</h3>
 			<MeetingTiles />
 		</GlobalColContainer>
 		<GlobalColContainer>
