ad12e3eb5f43a3b66705b8fdd657fbd5e8d47765
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-11-28 14:09:55 +0100

    meeting question with context - admin + themes

--- src/components/Meeting/MeetingSettings.jsx
+++ src/components/Meeting/MeetingSettings.jsx
@@ -5,15 +5,22 @@ import { MeetingInformations } from "./MeetingInformations";
 import { MeetingButton } from "./MeetingButton";
 
 export function	MeetingSettings({ setGenerate, currQuestion, setCurrQuestion }) {
+	const	[context, setContext] = useState({
+		administrations: [],
+		themes: [],
+	});
+	
 	return <GlobalRowContainer extraClass='fr-grid-row--center'>
 		<GlobalDiv>
 			<MeetingInformations
 				setCurrQuestion={setCurrQuestion}
+				setContext={setContext}
 			/>
 			<MeetingButton
 				isDisable={currQuestion.length === 0}
 				currQuestion={currQuestion}
 				setGenerate={setGenerate}
+				context={context}
 			/>
 		</GlobalDiv>
 	</GlobalRowContainer>
