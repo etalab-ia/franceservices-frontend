6cccf32fede9c2d2420002e6dc00f96320bf07e0
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-12-05 18:32:44 +0100

    clean reducers

--- src/utils/reducer/archive.jsx
+++ src/utils/reducer/archive.jsx
@@ -1,65 +1,32 @@
 export const	archiveReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'SET_ARCHIVE': {
-			if (state.length === 0)
-				return state;
-	  
-			const	lastIndex = state.length - 1;
-			const	source = state[lastIndex].question.model_name === 'albert-light' ? 'service-public.fr' : 'transformation.gouv.fr';
+			const	lastIndex = state.length ? state.length - 1 : state.length;
 	  
 			return [
-			  	...state.slice(0, lastIndex),
+			  	...state,
 			  	{
 					...state[lastIndex],
-					choices: action.nextChoices,
 					date: action.nextDate,
-					tags:action.nextTags,
-					source: source,
-					agentResponse: action.nextAgentResponse,
+					tags: action.nextTags,
+					source: 'service-public.fr',
+					messages: action.nextMessages,
+					type: action.nextType,
 					sheets: action.nextSheets,
-					type: action.nextType
+					limit: 7,
 			 	},
 			];
 		}
-		case 'SET_ARCHIVE_QUESTION': {
-			const	lastIndex = state.length - 1;
-		
-			return [
-				...state.slice(0, lastIndex),
-				{
-					...state[lastIndex],
-					question: action.nextQuestion
-				},
-		  ];
-		}
 		case 'SET_ARCHIVE_LIMIT': {
-			const	lastIndex = state.length - 1;
+			const	lastIndex = state.length ? state.length - 1 : state.length;
 
 			return [
 				...state.slice(0, lastIndex),
 				{
 					...state[lastIndex],
-					question: {
-						...state.question,
-						limit: action.nextLimit
-					}
+					limit: action.nextLimit,
 				},
 		  ];
-		}
-		case 'SET_ARCHIVE_MESSAGES': {
-			if (state.length === 0)
-				return state;
-	  
-			const	lastIndex = state.length - 1;
-
-			return [
-				...state.slice(0, lastIndex),
-				{
-					...state[lastIndex],
-					agentResponse: action.nextAgentResponse,
-					question: action.nextQuestion,
-				}
-			]
 		}
 	  	default: { return state };
 	}
