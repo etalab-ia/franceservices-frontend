0331c52a965940c4bd82bb158dd1240fa8602eac
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-12-21 19:57:38 +0100

    Fix meeting page when no chunks

--- src/components/Global/GlobalParagraph.jsx
+++ src/components/Global/GlobalParagraph.jsx
@@ -1,3 +1,5 @@
+import Linkify from 'react-linkify';
+
 export const GlobalParagraph = ({ children, extraClass }) => {
 	return (
 	  	<p className={`text-justify fr-my-1w ${extraClass}`}>
@@ -5,10 +7,10 @@ export const GlobalParagraph = ({ children, extraClass }) => {
 				children
 			:
 				children.split('\n').map((line, lineIndex) => (
-					<span key={lineIndex}>
+					<Linkify target="_blank" key={lineIndex}>
 						{lineIndex > 0 && <br />}
 						{line}
-					</span>
+					</Linkify>
 				))
 			}
 		</p>
