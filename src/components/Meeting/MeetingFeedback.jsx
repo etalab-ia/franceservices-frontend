12f6cc55c6dfad2433b8ec893ee8ed565c5f1fd2
JehanneDussert <jehanne-dussert@hotmail.fr>
2023-12-21 20:18:30 +0100

    prepare feedback

--- src/components/Meeting/MeetingFeedback.jsx
+++ src/components/Meeting/MeetingFeedback.jsx
@@ -1,19 +1,46 @@
 import thumbsUp from "../../../icons/feedbacks/thumbsUp.svg";
 import thumbsDown from "../../../icons/feedbacks/thumbsDown.svg";
+import { useFetch } from "../../utils/hooks";
+import { feedbackUrl } from "../../constants/api";
+import { setHeaders } from "../../utils/setData";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { ButtonInformation } from "../Global/ButtonInformation";
+import { thankFeedback } from "../../constants/feedback";
 
 export const    MeetingFeedback = () => {
+	const	userToken = useSelector((state) => state.auth.userToken);
+	const	dispatch = useDispatch();
+	const	[isClicked, setIsClicked] = useState(false);
 
-	const	handleClick = () => {
+	const	handleClick = (isGood) => {
 		// TODO: WHEN BACK IS READY: sent boolean to /feedback endpoint
-		console.log('click');
+		
+		// const	data = {
+		// 	isGood: isGood,
+		// 	message: '',
+		// 	reasons: ''
+		// }
+
+		// useFetch(`${feedbackUrl}/${chat_id}/${stream_id}`, 'POST', {
+		// 	data: data,
+		// 	headers: setHeaders(userToken, false)	
+		// }, dispatch);
+		
+		setIsClicked(true);
+
+		setTimeout(() => {
+			setIsClicked(false);
+		}, 5000);
 	}
 
 	return <div className="fr-mt-2w">
-		<button onClick={handleClick} className="fr-mr-1w border border-[#DDD]">
+		<button onClick={() => handleClick(true)} className="fr-mr-1w border border-[#DDD]">
 			<img className="fr-m-1w" src={thumbsUp} alt="Feedback positif"/>
 		</button>
-		<button onClick={handleClick} className="border border-[#DDD]">
+		<button onClick={() => handleClick(false)} className="border border-[#DDD]">
 			<img className="fr-m-1w" src={thumbsDown} alt="Feedback négatif"/>
 		</button>
+		{isClicked && <ButtonInformation>{thankFeedback}</ButtonInformation>}
 	</div>
 }
