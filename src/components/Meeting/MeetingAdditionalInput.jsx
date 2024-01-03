59026abff905507b4a4d296ca918ec48380d88e6
Joe <cueille@protonmail.com>
2024-01-03 18:25:03 +0100

    Cleaned

--- src/components/Meeting/MeetingAdditionalInput.jsx
+++ src/components/Meeting/MeetingAdditionalInput.jsx
@@ -4,39 +4,36 @@ import { MeetingTags } from "./MeetingTags";
 import { MeetingInput } from "./MeetingInput";
 
 export function MeetingAdditionalInput({ context }) {
-	const	[themes, setThemes] = useState([]);
-	const	[administrations, setAdministrations] = useState([]);
+    const [themes, setThemes] = useState([]);
+    const [administrations, setAdministrations] = useState([]);
 
-	useEffect(() => {
-		setThemes(context.themes);
-		setAdministrations(context.administrations);
-	}, [context]);
-	    const handleSetTag = (tag, fieldName) => {
-        if (fieldName === "themes") {
-            setThemes([...themes, tag]);
-        } else if (fieldName === "administrations") {
-            setAdministrations([...administrations, tag]);
-        }
+    useEffect(() => {
+        setThemes(context.themes);
+        setAdministrations(context.administrations);
+    }, [context]);
+
+    const handleSetTag = (tag, fieldName) => {
+        const updateState = fieldName === "themes" ? setThemes : setAdministrations;
+        updateState(currentTags => [...currentTags, tag]);
     };
+
     return (
         <div className="fr-mt-2w">
-            {inputFields.map((field, index) => {
-                return (
-                    <div className="fr-mb-4w" key={index}>
-                        <MeetingInput
-                            field={field}
-                            onTagSelect={handleSetTag}
-                            themes={themes}
-                            administrations={administrations}
-                        />
-                        <MeetingTags
-                            tags={field.name === "themes" ? themes : administrations}
-                            setTags={field.name === "themes" ? setThemes : setAdministrations}
-                            field={field}
-                        />
-                    </div>
-                );
-            })}
+            {inputFields.map((field, index) => (
+                <div className="fr-mb-4w" key={index}>
+                    <MeetingInput
+                        field={field}
+                        onTagSelect={handleSetTag}
+                        themes={themes}
+                        administrations={administrations}
+                    />
+                    <MeetingTags
+                        tags={field.name === "themes" ? themes : administrations}
+                        setTags={field.name === "themes" ? setThemes : setAdministrations}
+                        field={field}
+                    />
+                </div>
+            ))}
         </div>
     );
 }
