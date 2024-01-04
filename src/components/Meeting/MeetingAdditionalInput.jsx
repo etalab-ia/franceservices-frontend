14e230a519ea612ac7d697dab8a4f6f04593b845
JehanneDussert <jehanne-dussert@hotmail.fr>
2024-01-04 12:15:14 +0100

    fix tags when user post it

--- src/components/Meeting/MeetingAdditionalInput.jsx
+++ src/components/Meeting/MeetingAdditionalInput.jsx
@@ -1,30 +1,21 @@
-import { useEffect, useState } from "react";
 import { inputFields } from "../../constants/meetingInputFields";
 import { MeetingTags } from "./MeetingTags";
 import { MeetingInput } from "./MeetingInput";
 
-export function MeetingAdditionalInput({ context }) {
-	const	[themes, setThemes] = useState([]);
-	const	[administrations, setAdministrations] = useState([]);
-
-	useEffect(() => {
-		setThemes(context.themes);
-		setAdministrations(context.administrations);
-	}, [context]);
-
+export function MeetingAdditionalInput({ context, setContext }) {
 	return <div className="fr-mt-2w">
 		{inputFields.map((field, index) => {
-			const	tags = field.name === "themes" ? themes : administrations;
-			const	setTags = field.name === "themes" ? setThemes : setAdministrations;
+			const	tags = field.name === "themes" ? context.themes : context.administrations;
 
 			return <div className="fr-mb-4w" key={index}>
 				<MeetingInput
 					field={field}
 				/>
 				<MeetingTags
-					tags={tags}
-					setTags={setTags}
+					setContext={setContext}
+					context={context}
 					field={field}
+					tags={tags}
 				/>
 			</div>
 		})}
