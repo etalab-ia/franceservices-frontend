a5c68cccac8dee5cf93ce16a231649c17c0c5bae
JehanneDussert <jehanne-dussert@hotmail.fr>
2024-01-05 17:38:39 +0100

    feedback done

--- src/components/Chat/ChatFollowUp.tsx
+++ src/components/Chat/ChatFollowUp.tsx
@@ -3,14 +3,14 @@ import { DisplayStream } from "../Stream/DisplayStream"
 import { NewQuestion } from "./NewQuestion"
 import { GlobalRowContainer } from "../Global/GlobalRowContainer"
 import { AvatarToolsContainer } from "./AvatarToolsContainer"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Feedback, InitialFeedback } from "../../utils/feedback";
 
 export function ChatFollowUp({ stream, tabs }) {
 	const [feedback, setFeedback] = useState<Feedback>(InitialFeedback)
 	const conditionDiv =
 		(stream.response.length !== 0 || stream.historyStream.length !== 0) && tabs.activeTab === 0
-	// const newQuestionCondition = !stream.isStreaming && feedback.isConfirmed
+	const newQuestionCondition = !stream.isStreaming && feedback.isConfirmed
 
 	return (
 		<>
@@ -21,7 +21,7 @@ export function ChatFollowUp({ stream, tabs }) {
 						<DisplayStream stream={stream} tabs={tabs} />
 					</GlobalRowContainer>
 					{!stream.isStreaming && <UserExperience feedback={feedback} setFeedback={setFeedback}/>}
-					{/* {newQuestionCondition && <NewQuestion />} */}
+					{newQuestionCondition && <NewQuestion />}
 				</div>
 			)}
 		</>
