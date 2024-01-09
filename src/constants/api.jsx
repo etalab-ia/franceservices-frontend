99c300455715f84d9b8184064d96ce53adc3e2af
JehanneDussert <jehanne-dussert@hotmail.fr>
2024-01-09 17:42:29 +0100

    deploy MFS branch

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,4 +1,4 @@
-const	apiBase = "https://albert.etalab.gouv.fr/api/v2";
+const	apiBase = "https://franceservices.etalab.gouv.fr/api/v2";
 // const   apiBase = "http://127.0.0.1:8090";
 // v3 à venir
 export const	apiUrl = apiBase + "/stream";
