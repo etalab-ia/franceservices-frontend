3434d995b384a214d29bfa1b632004fbc5c2bb18
Joe <cueille@protonmail.com>
2024-01-10 12:29:22 +0100

    Fixed login page flashing when refreshing. Fixed systematic redirection to home when refreshing

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,34 +1,37 @@
-import { Header } from "@codegouvfr/react-dsfr/Header"
 import { Badge } from "@codegouvfr/react-dsfr/Badge"
-import { Chatbot } from "../pages/Chatbot"
-import { Routes, Route, Navigate } from "react-router-dom"
-import { navFunc } from "../constants/router"
-import { Login } from "../pages/Login"
-import { Signup } from "../pages/Signup"
-import { ResetPassword } from "../pages/ResetPassword"
-import { NewPassword } from "../pages/NewPassword"
-import { quickAccessItemsFunc } from "../constants/header"
+import { headerFooterDisplayItem } from "@codegouvfr/react-dsfr/Display"
+import { Footer } from "@codegouvfr/react-dsfr/Footer"
+import { Header } from "@codegouvfr/react-dsfr/Header"
 import { useEffect, useState } from "react"
-import { checkConnexion } from "../utils/localStorage"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { quickAccessItemsFunc } from "../constants/header"
+import { navFunc } from "../constants/router"
+import { Chatbot } from "../pages/Chatbot"
+import { Contact } from "../pages/Contact"
 import { History } from "../pages/History"
 import { Home } from "../pages/Home"
-import { Footer } from "@codegouvfr/react-dsfr/Footer"
-import { headerFooterDisplayItem } from "@codegouvfr/react-dsfr/Display"
+import { Login } from "../pages/Login"
 import { Meeting } from "../pages/Meeting"
-import { Contact } from "../pages/Contact"
+import { NewPassword } from "../pages/NewPassword"
+import { ResetPassword } from "../pages/ResetPassword"
+import { Signup } from "../pages/Signup"
 import { InitialUserAuth, UserAuth } from "../utils/auth"
 import { useAppDispatch } from "../utils/hooks"
-import { rmAuth } from "../utils/manageConnexion"
+import { checkConnexion } from "../utils/localStorage"
 
 export const Root = () => {
 	const navigationData = navFunc()
 	const [userAuth, setUserAuth] = useState<UserAuth>(InitialUserAuth)
 	const [authFailed, setAuthFailed] = useState(false)
 	const dispatch = useAppDispatch()
-
+	const [isLoading, setIsLoading] = useState(true)
 	useEffect(() => {
-		checkConnexion(setUserAuth)
+		checkConnexion(setUserAuth).finally(() => setIsLoading(false))
 	}, [dispatch])
+
+	if (isLoading) {
+		return <div className="bg-red"></div>
+	}
 
 	return (
 		<div className="h-screen" id="screen">
