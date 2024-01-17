41681657b9f38a6bdbe29837b66cf10c75b325eb
Joe Slain <cueille@protonmail.com>
2024-01-17 23:42:57 +0100

    Typed stream reducer

--- types.ts
+++ types.ts
@@ -88,3 +88,10 @@ export type ArchiveType = {
 	webservices: []
 	chunks: any[]
 }
+
+export type StreamState = {
+	response: string[]
+	historyStream: string[]
+	isStreaming: boolean
+	activeTab: number
+}
