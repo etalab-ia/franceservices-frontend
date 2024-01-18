a7eba2bb5d07f70d0ff46a6eb00cc5b5d9eb98bf
Joe Slain <cueille@protonmail.com>
2024-01-18 13:16:38 +0100

    Responsive faq

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -11,18 +11,14 @@ export function FAQ() {
 				<div className="flex flex-col flex-grow">
 					<h6 className="font-bold text-2xl fr-pb-2w">Categorie 1</h6>
 
-					<Accordion label="titre suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuupeeeeeeeeeer loooong">
-						Content of the Accordion 1
-					</Accordion>
+					<Accordion label="Name of the Accordion 1">Content of the Accordion 1</Accordion>
 					<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
 					<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
 					<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
 				</div>
 				<div className="flex flex-col flex-grow accordion">
 					<h6 className="font-bold text-2xl fr-pb-2w">Categorie 1</h6>
-					<Accordion label="titre suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuupeeeeeeeeeer loooong">
-						Content of the Accordion 1
-					</Accordion>
+					<Accordion label="Name of the Accordion 1">Content of the Accordion 1</Accordion>
 					<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
 					<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
 					<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
