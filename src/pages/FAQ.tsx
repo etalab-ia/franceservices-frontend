04a15b8f712f21292ddb8476f82408cfca9d3e0a
Joe Slain <cueille@protonmail.com>
2024-01-18 13:11:28 +0100

    Responsive faq

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -7,24 +7,25 @@ export function FAQ() {
 		<div className="fr-container fr-p-4w h-full">
 			<GlobalTitle>Aide</GlobalTitle>
 			<p className="fr-pb-2w">Des réponses aux questions fréquemment posées sur Albert</p>
-			<div className="flex w-full flex-grow">
+			<div className="flex accordion-container w-full">
 				<div className="flex flex-col flex-grow">
 					<h6 className="font-bold text-2xl fr-pb-2w">Categorie 1</h6>
-					<div className={fr.cx("fr-accordions-group")}>
-						<Accordion label="Name of the Accordion 1">Content of the Accordion 1</Accordion>
-						<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
-						<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
-						<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
-					</div>
+
+					<Accordion label="titre suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuupeeeeeeeeeer loooong">
+						Content of the Accordion 1
+					</Accordion>
+					<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
+					<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
+					<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
 				</div>
-				<div className="flex flex-col flex-grow fr-pl-2w">
+				<div className="flex flex-col flex-grow accordion">
 					<h6 className="font-bold text-2xl fr-pb-2w">Categorie 1</h6>
-					<div className={fr.cx("fr-accordions-group")}>
-						<Accordion label="Name of the Accordion 1">Content of the Accordion 1</Accordion>
-						<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
-						<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
-						<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
-					</div>
+					<Accordion label="titre suuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuupeeeeeeeeeer loooong">
+						Content of the Accordion 1
+					</Accordion>
+					<Accordion label="Name of the Accordion 2">Content of the Accordion 2</Accordion>
+					<Accordion label="Name of the Accordion 3">Content of the Accordion 3</Accordion>
+					<Accordion label="Name of the Accordion 4">Content of the Accordion 4</Accordion>
 				</div>
 			</div>
 		</div>
