fac71f7e48377a79606917a1b21b4332b52badd3
Joe <cueille@protonmail.com>
2024-02-05 20:10:06 +0100

    FOnt size changes and separator, branching to add new interface

--- src/components/Meeting/MeetingPromptAdvice.tsx
+++ src/components/Meeting/MeetingPromptAdvice.tsx
@@ -7,6 +7,8 @@ import {
 import { GlobalColContainer } from '../Global/GlobalColContainer'
 import { GlobalParagraph } from '../Global/GlobalParagraph'
 import { GlobalRowContainer } from '../Global/GlobalRowContainer'
+import thumbsDown from '../../../icons/feedbacks/thumbsDown.svg'
+import thumbsUp from '../../../icons/feedbacks/thumbsUp.svg'
 
 /*
  * Advices and examples of good and bad prompts
@@ -28,31 +30,29 @@ export function MeetingPromptAdvice({}) {
  */
   return (
     <>
-      <GlobalColContainer>
-        <div>{meetingInputGuidelines}</div>
-        <div className={'flex '} onClick={handleToggleAll}>
-          {meetingExamplesTitle}
-          <span
-            className={`fr-text-mention--grey fr-icon--sm fr-icon-arrow-${
-              isAccordionOpen ? 'up' : 'down'
-            }-s-line fr-my-1v`}
-          ></span>
-        </div>
-        {meetingPromptExamples.map((ex, index) => (
-          <div
-            className="fr-mt-1w"
-            key={index}
-            style={{ display: isAccordionOpen ? 'block' : 'none' }}
-          >
-            <div className="flex content-center items-center justify-start">
-              <img className="w-6 h-6 mr-2" src={ex.img} alt={ex.alt} />
-              <p className="self-center">{ex.title}</p>
-            </div>
+      <div>{meetingInputGuidelines}</div>
+      <div className={'flex '} onClick={handleToggleAll}>
+        {meetingExamplesTitle}
+        <span
+          className={`fr-text-mention--grey fr-icon--sm fr-icon-arrow-${
+            isAccordionOpen ? 'up' : 'down'
+          }-s-line fr-my-1v`}
+        ></span>
+      </div>
+      {meetingPromptExamples.map((ex, index) => (
+        <div
+          className="fr-mt-1w"
+          key={index}
+          style={{ display: isAccordionOpen ? 'block' : 'none' }}
+        >
+          <div className="flex ">
+            <img src={ex.img} alt={ex.alt} className="mr-2" />
 
-            <p className="mb-4 text-justify text-xs">{ex.description}</p>
+            {ex.title}
           </div>
-        ))}
-      </GlobalColContainer>
+          <p className="mb-4 text-justify fr-text--xs">{ex.description}</p>
+        </div>
+      ))}
     </>
   )
 }
