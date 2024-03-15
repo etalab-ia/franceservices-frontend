1d606085f2158fdb3dcdd51f64972ff1d1ef54b6
Joe <cueille@protonmail.com>
2024-03-15 14:30:02 +0100

    fix: visual difference between streming and non-streaming response

--- src/components/Global/GlobalParagraph.tsx
+++ src/components/Global/GlobalParagraph.tsx
@@ -5,7 +5,7 @@ export const GlobalParagraph = ({
   extraClass,
 }: { children: React.ReactNode; extraClass?: string }) => {
   return (
-    <div className={` fr-my-1w ${extraClass}`}>
+    <div className={`text-justify fr-my-1w ${extraClass}`}>
       {typeof children !== 'string'
         ? children
         : children.split('\n').map((line, lineIndex) => (
