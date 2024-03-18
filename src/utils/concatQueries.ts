9073405dd1596743927aa5c75663f527c05f4f31
Joe <cueille@protonmail.com>
2024-03-18 18:56:49 +0100

    feature: add concat function to improve conversationnal abilities of Albert

---
+++ src/utils/concatQueries.ts
@@ -0,0 +1,21 @@
+import { UserHistory } from '@types'
+
+export function concatQueries(
+  currentQuery: string,
+  previousQueries: UserHistory[]
+): string {
+  let result = ''
+
+  for (let i = previousQueries.length - 1; i >= 0; i--) {
+    //console.log('concatQueries', i, previousQueries[i].query)
+    if (i === previousQueries.length - 1) {
+      if (previousQueries[i].query.length + currentQuery.length + 1 > 4096)
+        return currentQuery
+      result = previousQueries[i].query + '\n' + currentQuery
+    } else {
+      if (previousQueries[i].query.length + result.length + 1 > 4096) return result
+      result = previousQueries[i].query + '\n' + result
+    }
+  }
+  return result
+}
