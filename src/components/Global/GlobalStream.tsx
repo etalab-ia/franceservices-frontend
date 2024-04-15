4a38ed8be07b5d9f6f93311809e5d9ea2b497843
Joe Slain <cueille@protonmail.com>
2024-04-15 18:18:23 +0200

    feat: turn stream.response into a string

--- src/components/Global/GlobalStream.tsx
+++ src/components/Global/GlobalStream.tsx
@@ -1,19 +1,18 @@
 export const GlobalStream = ({
   response,
   extraClass,
-}: { response: any[]; extraClass?: string }) => {
+}: { response: string; extraClass?: string }) => {
   return (
     <div className={`fr-my-1w ${extraClass}`}>
-      {response.map((item, index) => (
-        <span key={index}>
-          {item.split('\n').map((line, lineIndex) => (
-            <span key={lineIndex}>
-              {lineIndex > 0 && <br />}
-              {line}
-            </span>
-          ))}
-        </span>
-      ))}
+      <span>
+        {response.split('\n').map((line, lineIndex) => (
+          <span key={lineIndex}>
+            {lineIndex > 0 && <br />}
+            {line}
+          </span>
+        ))}
+      </span>
+      )
     </div>
   )
 }
