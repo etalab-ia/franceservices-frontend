36eb1d6715aa2d9602c5cfbce10d0daf7a5da5df
Joe <cueille@protonmail.com>
2024-04-17 18:08:48 +0200

    feat: SourceTooltip remove duplicated anchor ids

---
+++ src/components/Global/CurrentStream.tsx
@@ -0,0 +1,41 @@
+import { TextWithSources } from '../Sources/TextWithSources'
+export const CurrentStream = ({
+  response,
+  extraClass,
+}: { response: string; extraClass?: string }) => {
+  return (
+    <div className={`fr-my-1w${extraClass}`}>
+      <span>
+        {response.split('\n').map((line, lineIndex) => (
+          <span key={lineIndex}>
+            {lineIndex > 0 && <br />}
+            <TextWithSources text={line} sources={[]} />
+          </span>
+        ))}
+      </span>
+      )
+    </div>
+  )
+}
+
+/*
+export const CurrentStream = ({
+  response,
+  extraClass,
+}: { response: string; extraClass?: string }) => {
+  return (
+    <div className={`fr-my-1w ${extraClass}`}>
+      <span>
+        {response.split('\n').map((line, lineIndex) => (
+          <span key={lineIndex}>
+            {lineIndex > 0 && <br />}
+            {line}
+          </span>
+        ))}
+      </span>
+      )
+    </div>
+  )
+}
+
+*/
---
+++ src/components/Sources/SourceTooltip.tsx
@@ -0,0 +1,79 @@
+import { Tooltip } from 'react-tooltip'
+
+export function SourceTooltip({
+  id,
+  text,
+  source,
+  sourceSite,
+  sheetUrl,
+}: {
+  id: string
+  text: string
+  source: string
+  sourceSite: string
+  sheetUrl: string
+}) {
+  return (
+    <span className="fr-ml-1v" style={{ position: 'relative', display: 'inline' }}>
+      <a
+        href={sheetUrl}
+        id={id}
+        style={{ textDecoration: 'none', display: 'inline', color: 'inherit' }}
+      >
+        <span
+          className="fr-icon-quote-fill fr-text-action-high--blue-cumulus fr-mr-2v"
+          style={{ display: 'inline' }}
+        />
+      </a>
+      <Tooltip
+        place="bottom"
+        opacity={1}
+        style={{
+          position: 'absolute',
+          zIndex: 1000,
+          backgroundColor: 'white',
+          borderRadius: '0px',
+          color: 'black',
+          boxShadow:
+            'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+          opacity: 1,
+        }}
+        anchorSelect={`#${id}`}
+        clickable
+        noArrow
+      >
+        <Source text={text} source={source} sourceSite={sourceSite} sheetUrl={sheetUrl} />
+      </Tooltip>
+    </span>
+  )
+}
+
+function Source({
+  text,
+  source,
+  sourceSite,
+  sheetUrl,
+}: { text: string; source: string; sourceSite: string; sheetUrl: string }) {
+  return (
+    <div className="fr-p-2w inline-block max-w-[392px] text-wrap break-words">
+      <p className="fr-mb-1w fr-text--sm fr-text-mention--grey">
+        Passage utilisé pour générer cette phrase
+      </p>
+      <p className="fr-text--lg">{text}</p>
+      <p className="fr-mb-1w">Extrait de: {source}</p>
+      <div className="fr-grid-row w-full">
+        <p className="fr-badge fr-badge--sm fr-badge--info fr-badge--no-icon">
+          {sourceSite}
+        </p>
+        <a
+          className="external-link-icon ml-auto no-underline"
+          href={sheetUrl}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="fr-icon-arrow-right-line" />
+        </a>
+      </div>
+    </div>
+  )
+}
