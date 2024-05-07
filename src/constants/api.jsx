bb72c433dccc2cc61fe801fab03d6e5545c394c1
Joe <cueille@protonmail.com>
2024-05-07 15:47:06 +0200

    style: remove sensitive data

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,18 +1,19 @@
 // POST USER QUESTION / GET AGENT RESPONSE
-export const	apiUrl = "http://***REMOVED***/api/v2/stream"
+export const apiUrl = "http://***REMOVED***/api/v2/stream";
 
 // GET INSTITUTIONS
-export  const	institutionsUrl = "http://***REMOVED***/api/v2/institutions"
+export const institutionsUrl = "http://***REMOVED***/api/v2/institutions";
 
 // GET INDEXES
-export const    indexesUrl = "http://***REMOVED***/api/v2/indexes";
+export const indexesUrl = "http://***REMOVED***/api/v2/indexes";
 
 // STOP GENERATION
-export const	stopGenerationUrl = "http://***REMOVED***/api/api/fabrique_stop"
+export const stopGenerationUrl = "http://***REMOVED***/api/api/fabrique_stop";
 
 // SIGNIN
-export const	signinUrl = "http://***REMOVED***/api/v2/sign_in"
-export const	signoutUrl = "http://***REMOVED***/api/v2/sign_out"
-export const	resetPasswordMailUrl = "http://***REMOVED***/api/v2/send_reset_password_email"
-export const	resetPasswordUrl = "http://***REMOVED***/api/v2/reset_password"
-export const    userUrl = "http://***REMOVED***/api/v2/user/me"
+export const signinUrl = "http://***REMOVED***/api/v2/sign_in";
+export const signoutUrl = "http://***REMOVED***/api/v2/sign_out";
+export const resetPasswordMailUrl =
+	"http://***REMOVED***/api/v2/send_reset_password_email";
+export const resetPasswordUrl = "http://***REMOVED***/api/v2/reset_password";
+export const userUrl = "http://***REMOVED***/api/v2/user/me";
