32d53bf5abdf19ec6e774b8c7ae8ce858eacb57d
Joe Slain <cueille@protonmail.com>
2024-06-03 20:05:58 +0200

    feat: begin implementing new homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,20 +10,42 @@ import { useContext } from 'react'
 import { GlobalDiv } from '../components/Global/GlobalDiv'
 import { GlobalTitle } from '../components/Global/GlobalTitle'
 import { HomeTiles } from '../components/Home/HomeTiles'
+import { fr } from '@codegouvfr/react-dsfr'
 
 export function Home() {
   const isMFS = useContext(isMFSContext)
   const tiles = isMFS ? MFSressourcesTiles : generalistRessourcesTiles
-
+  const chat = {
+    title: 'Chat',
+    numberOfMessages: 3,
+  }
   return (
     <div className="fr-container">
       <GlobalDiv>
+        <ChatListRow chat={chat} />
         <GlobalTitle>{toolsTitle}</GlobalTitle>
         <HomeTiles tiles={isMFS ? MFStoolsTiles : toolsTiles} />
       </GlobalDiv>
       <div className="fr-mb-12w">
         <GlobalTitle>{ressourcesTitle}</GlobalTitle>
         <HomeTiles tiles={tiles} />
+      </div>
+    </div>
+  )
+}
+
+function ChatListRow({ chat }) {
+  return (
+    <div className="fr-grid-row bg-red-500">
+      <div>
+        <p className="fr-text--lg">{chat.title}</p>
+      </div>
+      <div>
+        <div>
+          <p className="fr-tag fr-tag--sm fr-text-action-high--blue-france fr-background-action-low--blue-france">
+            {chat.numberOfMessages} messages
+          </p>
+        </div>
       </div>
     </div>
   )
