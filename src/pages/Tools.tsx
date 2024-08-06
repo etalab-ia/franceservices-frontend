4a4a8e78d8fcae05ef79da5102c46f1bb025e08a
Joe Slain <cueille@protonmail.com>
2024-08-06 19:15:51 +0200

    fixing images

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -3,6 +3,7 @@ import { GlobalTitle } from '../components/Global/GlobalTitle'
 import { HomeTiles } from '../components/Home/HomeTiles'
 
 export function Tools() {
+  console.log('logos')
   return (
     <div className="fr-container fr-mb-12w flex flex-col gap-4 fr-pt-4w">
       <img src="../../artwork/logo/LogoA+.png" alt="Logo A+" className="w-24 h-24" />
