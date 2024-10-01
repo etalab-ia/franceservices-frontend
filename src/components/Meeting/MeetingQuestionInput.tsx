4bc856f1e81bc659bc231e8a974d9da5961079f1
Joe Slain <cueille@proton.me>
2024-10-01 15:10:20 +0200

    fix: empty additional input when question is sent

--- src/components/Meeting/MeetingQuestionInput.tsx
+++ src/components/Meeting/MeetingQuestionInput.tsx
@@ -110,7 +110,12 @@ export function MeetingQuestionInput({
         chatId,
         user.lastStreamId,
       )
+
       setQuestionInput('')
+      setContext({
+        administrations: [],
+        themes: [],
+      })
     }
   }
 
