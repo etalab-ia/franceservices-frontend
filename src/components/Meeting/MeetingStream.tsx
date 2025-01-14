15953e395653d587a389a087d9eeb72b27d525a0
Joe Slain <cueille@protonmail.com>
2025-01-14 17:26:52 +0100

    feat: useStreamText

--- src/components/Meeting/MeetingStream.tsx
+++ src/components/Meeting/MeetingStream.tsx
@@ -5,53 +5,18 @@ import { TextWithSources } from 'components/Sources/TextWithSources'
 import { GlobalParagraph } from 'components/Global/GlobalParagraph'
 import Separator from 'components/Global/Separator'
 import { Feedback } from '../Feedbacks/Feedback'
+import { useStreamText } from 'hooks/useStreamText'
 
 export function MeetingStream() {
   const stream = useSelector((state: RootState) => state.stream)
   const agentResponse = stream.historyStream[0]
-
-  // For letter-by-letter text
-  const [displayedText, setDisplayedText] = useState('')
-  const lastIndexRef = useRef(0)
-
-  // We'll detect a new question/stream by checking user.lastStreamId
   const user = useSelector((state: RootState) => state.user)
-  const [prevStreamId, setPrevStreamId] = useState<number | null>(null)
 
-  // 1) Reset on new question
-  useEffect(() => {
-    // If lastStreamId changed => a brand-new question is being asked
-    if (user.lastStreamId && user.lastStreamId !== prevStreamId) {
-      setDisplayedText('')
-      lastIndexRef.current = 0
-      setPrevStreamId(user.lastStreamId)
-    }
-  }, [user.lastStreamId, prevStreamId])
-
-  // 2) Append text while streaming
-  useEffect(() => {
-    if (!stream.isStreaming) return
-
-    const intervalId = setInterval(() => {
-      if (lastIndexRef.current < stream.response.length) {
-        setDisplayedText((prev) => {
-          const nextChar = stream.response[lastIndexRef.current]
-          lastIndexRef.current += 1
-          return prev + nextChar
-        })
-      }
-    }, 0) // speed as desired
-
-    return () => clearInterval(intervalId)
-  }, [stream.isStreaming, stream.response])
-
-  // 3) Once streaming is done, ensure full text is displayed
-  useEffect(() => {
-    if (!stream.isStreaming && stream.response) {
-      setDisplayedText(stream.response)
-      lastIndexRef.current = stream.response.length
-    }
-  }, [stream.isStreaming, stream.response])
+  const displayedText = useStreamText({
+    text: stream.response,
+    isStreaming: stream.isStreaming,
+    streamId: user.lastStreamId,
+  })
 
   return (
     <div>
